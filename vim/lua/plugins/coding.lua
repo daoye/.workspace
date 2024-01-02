717bc57e5cedef1ba0868b4da263790e352ca53c
Justin.Y.Sun <Justin.Y.Sun@newegg.com>
2024-01-02 09:29:37 +0800

    Fix comment option

--- vim/lua/plugins/coding.lua
+++ vim/lua/plugins/coding.lua
@@ -1,231 +1,235 @@
 return {
 
-  -- snippets
-  {
-    "L3MON4D3/LuaSnip",
-    build = (not jit.os:find("Windows"))
-        and "echo -e 'NOTE: jsregexp is optional, so not a big deal if it fails to build\n'; make install_jsregexp"
-        or nil,
-    dependencies = {
-      "rafamadriz/friendly-snippets",
-      config = function()
-        require("luasnip.loaders.from_vscode").lazy_load()
-      end,
+    -- snippets
+    {
+        "L3MON4D3/LuaSnip",
+        build = (not jit.os:find("Windows"))
+            and "echo -e 'NOTE: jsregexp is optional, so not a big deal if it fails to build\n'; make install_jsregexp"
+            or nil,
+        dependencies = {
+            "rafamadriz/friendly-snippets",
+            config = function()
+                require("luasnip.loaders.from_vscode").lazy_load()
+            end,
+        },
+        opts = {
+            history = true,
+            delete_check_events = "TextChanged",
+        },
+        -- stylua: ignore
+        keys = {
+            {
+                "<tab>",
+                function()
+                    local luasnip = require("luasnip")
+                    -- return luasnip.expand_or_locally_jumpable() and luasnip.expand_or_jump() or '<tab>'
+                    return luasnip.jumpable(1) and "<Plug>luasnip-jump-next" or "<tab>"
+                end,
+                expr = true,
+                silent = true,
+                mode = "i",
+                desc = "luasnip jump next"
+            },
+            { "<tab>",   function() require("luasnip").jump(1) end,  mode = "s",          desc = "luasnip jump next" },
+            { "<s-tab>", function() require("luasnip").jump(-1) end, mode = { "i", "s" }, desc = "luasnip jump prev" },
+        },
     },
-    opts = {
-      history = true,
-      delete_check_events = "TextChanged",
+
+
+    -- auto pairs
+    {
+        "echasnovski/mini.pairs",
+        event = "VeryLazy",
+        opts = {},
     },
-    -- stylua: ignore
-    keys = {
-      {
-        "<tab>",
-        function()
-          local luasnip = require("luasnip")
-          -- return luasnip.expand_or_locally_jumpable() and luasnip.expand_or_jump() or '<tab>'
-          return luasnip.jumpable(1) and "<Plug>luasnip-jump-next" or "<tab>"
+
+    -- surround
+    {
+        "echasnovski/mini.surround",
+        keys = function(_, keys)
+            -- Populate the keys based on the user's options
+            local plugin = require("lazy.core.config").spec.plugins["mini.surround"]
+            local opts = require("lazy.core.plugin").values(plugin, "opts", false)
+            local mappings = {
+                { opts.mappings.add,            desc = "Add surrounding",                     mode = { "n", "v" } },
+                { opts.mappings.delete,         desc = "Delete surrounding" },
+                { opts.mappings.find,           desc = "Find right surrounding" },
+                { opts.mappings.find_left,      desc = "Find left surrounding" },
+                { opts.mappings.highlight,      desc = "Highlight surrounding" },
+                { opts.mappings.replace,        desc = "Replace surrounding" },
+                { opts.mappings.update_n_lines, desc = "Update `MiniSurround.config.n_lines`" },
+            }
+            mappings = vim.tbl_filter(function(m)
+                return m[1] and #m[1] > 0
+            end, mappings)
+            return vim.list_extend(mappings, keys)
         end,
-        expr = true,
-        silent = true,
-        mode = "i",
-        desc = "luasnip jump next"
-      },
-      { "<tab>",   function() require("luasnip").jump(1) end,  mode = "s",          desc = "luasnip jump next" },
-      { "<s-tab>", function() require("luasnip").jump(-1) end, mode = { "i", "s" }, desc = "luasnip jump prev" },
+        opts = {
+            mappings = {
+                add = "ys",            -- Add surrounding in Normal and Visual modes
+                delete = "ds",         -- Delete surrounding
+                find = "fs",           -- Find surrounding (to the right)
+                find_left = "Fs",      -- Find surrounding (to the left)
+                highlight = "hs",      -- Highlight surrounding
+                replace = "cs",        -- Replace surrounding
+                update_n_lines = "ns", -- Update `n_lines`
+            },
+        },
     },
-  },
 
+    -- comments
+    {
+        "echasnovski/mini.comment",
+        event = "VeryLazy",
+        opts = {
+            options = {
+                ignore_blank_line = true,
+            },
 
-  -- auto pairs
-  {
-    "echasnovski/mini.pairs",
-    event = "VeryLazy",
-    opts = {},
-  },
+            -- Module mappings. Use `''` (empty string) to disable one.
+            mappings = {
+                -- Toggle comment (like `gcip` - comment inner paragraph) for both
+                -- Normal and Visual modes
+                comment = "cc",
 
-  -- surround
-  {
-    "echasnovski/mini.surround",
-    keys = function(_, keys)
-      -- Populate the keys based on the user's options
-      local plugin = require("lazy.core.config").spec.plugins["mini.surround"]
-      local opts = require("lazy.core.plugin").values(plugin, "opts", false)
-      local mappings = {
-        { opts.mappings.add,            desc = "Add surrounding",                     mode = { "n", "v" } },
-        { opts.mappings.delete,         desc = "Delete surrounding" },
-        { opts.mappings.find,           desc = "Find right surrounding" },
-        { opts.mappings.find_left,      desc = "Find left surrounding" },
-        { opts.mappings.highlight,      desc = "Highlight surrounding" },
-        { opts.mappings.replace,        desc = "Replace surrounding" },
-        { opts.mappings.update_n_lines, desc = "Update `MiniSurround.config.n_lines`" },
-      }
-      mappings = vim.tbl_filter(function(m)
-        return m[1] and #m[1] > 0
-      end, mappings)
-      return vim.list_extend(mappings, keys)
-    end,
-    opts = {
-      mappings = {
-        add = "ys",            -- Add surrounding in Normal and Visual modes
-        delete = "ds",         -- Delete surrounding
-        find = "fs",           -- Find surrounding (to the right)
-        find_left = "Fs",      -- Find surrounding (to the left)
-        highlight = "hs",      -- Highlight surrounding
-        replace = "cs",        -- Replace surrounding
-        update_n_lines = "ns", -- Update `n_lines`
-      },
+                -- Toggle comment on current line
+                comment_line = "cc",
+
+                -- Toggle comment on visual selection
+                comment_visual = 'cc',
+
+                -- Define 'comment' textobject (like `dgc` - delete whole comment block)
+                -- textobject = 'gc',
+            },
+        },
     },
-  },
 
-  -- comments
-  {
-    "echasnovski/mini.comment",
-    event = "VeryLazy",
-    opts = {
-      -- Module mappings. Use `''` (empty string) to disable one.
-      mappings = {
-        -- Toggle comment (like `gcip` - comment inner paragraph) for both
-        -- Normal and Visual modes
-        comment = "cc",
-
-        -- Toggle comment on current line
-        comment_line = "cc",
-
-        -- Toggle comment on visual selection
-        comment_visual = 'cc',
-
-        -- Define 'comment' textobject (like `dgc` - delete whole comment block)
-        -- textobject = 'gc',
-      },
-    },
-  },
-
-  -- better text-objects
-  {
-    "echasnovski/mini.ai",
-    -- keys = {
-    --   { "a", mode = { "x", "o" } },
-    --   { "i", mode = { "x", "o" } },
-    -- },
-    event = "VeryLazy",
-    dependencies = { "nvim-treesitter-textobjects" },
-    opts = function()
-      local ai = require("mini.ai")
-      return {
-        n_lines = 500,
-        custom_textobjects = {
-          o = ai.gen_spec.treesitter({
-            a = { "@block.outer", "@conditional.outer", "@loop.outer" },
-            i = { "@block.inner", "@conditional.inner", "@loop.inner" },
-          }, {}),
-          f = ai.gen_spec.treesitter({ a = "@function.outer", i = "@function.inner" }, {}),
-          c = ai.gen_spec.treesitter({ a = "@class.outer", i = "@class.inner" }, {}),
-        },
-      }
-    end,
-    config = function(_, opts)
-      require("mini.ai").setup(opts)
-    end,
-  },
-
-  -- text align
-  {
-    "echasnovski/mini.align",
-    event = "VeryLazy",
-    version = false,
-    config = function(_, opts)
-      require("mini.align").setup(opts)
-    end,
-  },
-
-
-  -- auto completion
-  {
-    "hrsh7th/nvim-cmp",
-    version = false, -- last release is way too old
-    event = "InsertEnter",
-    dependencies = {
-      "hrsh7th/cmp-nvim-lsp",
-      "hrsh7th/cmp-buffer",
-      "hrsh7th/cmp-path",
-      "hrsh7th/cmp-cmdline",
-      "saadparwaiz1/cmp_luasnip",
-    },
-    opts = function()
-      local cmp = require("cmp")
-
-      return {
-        completion = {
-          completeopt = "menu,menuone,noinsert",
-        },
-        snippet = {
-          expand = function(args)
-            require("luasnip").lsp_expand(args.body)
-          end,
-        },
-        mapping = cmp.mapping.preset.insert({
-          ["<C-n>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
-          ["<C-p>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
-          ["<C-u>"] = cmp.mapping.scroll_docs(-4),
-          ["<C-d>"] = cmp.mapping.scroll_docs(4),
-          ["<C-Space>"] = cmp.mapping.complete(),
-          ["<C-e>"] = cmp.mapping.abort(),
-          ["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-          ["<S-CR>"] = cmp.mapping.confirm({
-            behavior = cmp.ConfirmBehavior.Replace,
-            select = true,
-          }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-        }),
-        sources = cmp.config.sources({
-          { name = "nvim_lsp" },
-          { name = "luasnip" },
-          { name = "buffer" },
-          { name = "path" },
-        }),
-        formatting = {
-          format = function(_, item)
-            local icons = require("conf").icons.kinds
-            if icons[item.kind] then
-              item.kind = icons[item.kind] .. item.kind
-            end
-            return item
-          end,
-        },
-        experimental = {
-          ghost_text = {
-            hl_group = "LspCodeLens",
-          },
-        },
-        enabled = function()
-          -- return vim.api.nvim_buf_get_option(0, "buftype") ~= "prompt" or require("cmp_dap").is_dap_buffer()
-          return vim.api.nvim_buf_get_option(0, "buftype") ~= "prompt"
+    -- better text-objects
+    {
+        "echasnovski/mini.ai",
+        -- keys = {
+        --   { "a", mode = { "x", "o" } },
+        --   { "i", mode = { "x", "o" } },
+        -- },
+        event = "VeryLazy",
+        dependencies = { "nvim-treesitter-textobjects" },
+        opts = function()
+            local ai = require("mini.ai")
+            return {
+                n_lines = 500,
+                custom_textobjects = {
+                    o = ai.gen_spec.treesitter({
+                        a = { "@block.outer", "@conditional.outer", "@loop.outer" },
+                        i = { "@block.inner", "@conditional.inner", "@loop.inner" },
+                    }, {}),
+                    f = ai.gen_spec.treesitter({ a = "@function.outer", i = "@function.inner" }, {}),
+                    c = ai.gen_spec.treesitter({ a = "@class.outer", i = "@class.inner" }, {}),
+                },
+            }
         end,
-      }
-    end,
-    init = function()
-      local cmp = require("cmp")
-      cmp.setup.cmdline("/", {
-        mapping = cmp.mapping.preset.cmdline(),
-        sources = {
-          { name = "buffer" },
-        },
-      })
+        config = function(_, opts)
+            require("mini.ai").setup(opts)
+        end,
+    },
 
-      cmp.setup.cmdline(":", {
-        mapping = cmp.mapping.preset.cmdline(),
-        sources = cmp.config.sources({
-          { name = "path" },
-        }, {
-          { name = "cmdline" },
-        }),
-      })
+    -- text align
+    {
+        "echasnovski/mini.align",
+        event = "VeryLazy",
+        version = false,
+        config = function(_, opts)
+            require("mini.align").setup(opts)
+        end,
+    },
 
-      require("cmp").setup.filetype({ "dap-repl", "dapui_watches", "dapui_hover" }, {
-        sources = {
-          { name = "dap" },
+
+    -- auto completion
+    {
+        "hrsh7th/nvim-cmp",
+        version = false, -- last release is way too old
+        event = "InsertEnter",
+        dependencies = {
+            "hrsh7th/cmp-nvim-lsp",
+            "hrsh7th/cmp-buffer",
+            "hrsh7th/cmp-path",
+            "hrsh7th/cmp-cmdline",
+            "saadparwaiz1/cmp_luasnip",
         },
-      })
-    end,
-  },
+        opts = function()
+            local cmp = require("cmp")
+
+            return {
+                completion = {
+                    completeopt = "menu,menuone,noinsert",
+                },
+                snippet = {
+                    expand = function(args)
+                        require("luasnip").lsp_expand(args.body)
+                    end,
+                },
+                mapping = cmp.mapping.preset.insert({
+                    ["<C-n>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
+                    ["<C-p>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
+                    ["<C-u>"] = cmp.mapping.scroll_docs(-4),
+                    ["<C-d>"] = cmp.mapping.scroll_docs(4),
+                    ["<C-Space>"] = cmp.mapping.complete(),
+                    ["<C-e>"] = cmp.mapping.abort(),
+                    ["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+                    ["<S-CR>"] = cmp.mapping.confirm({
+                        behavior = cmp.ConfirmBehavior.Replace,
+                        select = true,
+                    }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+                }),
+                sources = cmp.config.sources({
+                    { name = "nvim_lsp" },
+                    { name = "luasnip" },
+                    { name = "buffer" },
+                    { name = "path" },
+                }),
+                formatting = {
+                    format = function(_, item)
+                        local icons = require("conf").icons.kinds
+                        if icons[item.kind] then
+                            item.kind = icons[item.kind] .. item.kind
+                        end
+                        return item
+                    end,
+                },
+                experimental = {
+                    ghost_text = {
+                        hl_group = "LspCodeLens",
+                    },
+                },
+                enabled = function()
+                    -- return vim.api.nvim_buf_get_option(0, "buftype") ~= "prompt" or require("cmp_dap").is_dap_buffer()
+                    return vim.api.nvim_buf_get_option(0, "buftype") ~= "prompt"
+                end,
+            }
+        end,
+        init = function()
+            local cmp = require("cmp")
+            cmp.setup.cmdline("/", {
+                mapping = cmp.mapping.preset.cmdline(),
+                sources = {
+                    { name = "buffer" },
+                },
+            })
+
+            cmp.setup.cmdline(":", {
+                mapping = cmp.mapping.preset.cmdline(),
+                sources = cmp.config.sources({
+                    { name = "path" },
+                }, {
+                    { name = "cmdline" },
+                }),
+            })
+
+            require("cmp").setup.filetype({ "dap-repl", "dapui_watches", "dapui_hover" }, {
+                sources = {
+                    { name = "dap" },
+                },
+            })
+        end,
+    },
 
 }
